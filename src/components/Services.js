1b3bdd5941963b6690e62936bb8de09b8427250c
Bosna Marinel <Marinel.Bosna@ibm.com>
2023-05-24 19:42:17 +0300

    replace static html for tours

--- src/components/Services.js
+++ src/components/Services.js
@@ -5,7 +5,6 @@ const Services = () => {
   return (
     <section className="section services" id="services">
       <Title title="our" subtitle="services" />
-
       <div className="section-center services-center">
         {services.map((service) => {
           return (
--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import imageOne from "../src/images/tour-1.jpeg";
+import imageTwo from "../src/images/tour-2.jpeg";
+import imageThree from "../src/images/tour-3.jpeg";
+import imageFour from "../src/images/tour-4.jpeg";
+
 export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
@@ -29,5 +34,56 @@ export const services = [
     icon: "fas fa-socks fa-fw",
     title: "amazing comfort",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: imageOne,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    description:
+      " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime " +
+      "  reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "china",
+    period: "6 days",
+    price: "from $2100",
+  },
+  {
+    id: 2,
+    image: imageTwo,
+    date: "october 1th, 2020",
+    title: "best of java",
+    description:
+      " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime " +
+      "  reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "indonesia",
+    period: "11 days",
+    price: "from $1400",
+  },
+  {
+    id: 3,
+    image: imageThree,
+    date: "september 15th, 2020",
+    title: "explore hong kong",
+    description:
+      " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime " +
+      "  reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "hong kong",
+    period: "8 days",
+    price: "from $5000",
+  },
+  {
+    id: 4,
+    image: imageFour,
+    date: "december 5th, 2019",
+    title: "kenya highlights",
+    description:
+      " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime " +
+      "  reprehenderit eum quodexercitationem fugit, qui corporis.",
+    location: "kenya",
+    period: "20 days",
+    price: "from $3300",
   },
 ];
