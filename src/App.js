bebf60b63f61444c9f6646cc6e3cd6484293f432
Bosna Marinel <Marinel.Bosna@ibm.com>
2023-05-24 14:48:14 +0300

    remove files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import "./App.css";
-
 function App() {
-  return (
-    <div className="App">
-      <h1>backroads app</h1>
-    </div>
-  );
+  return <h1>backroads app</h1>;
 }
 
 export default App;
